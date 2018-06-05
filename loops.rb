b9b7be6a4f956979891a01f22267df2eaefe4e7e
caitlinmch <caitlinmch@me.com>
2018-06-05 20:54:56 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -15,4 +15,35 @@
 #   end
 # end
 #   dance
-# # end ends the method
+# # end ends the helper_method
+
+
+def dance 
+  move_completed=10
+  while moves_completed<10 
+  puts "To the left, to the left"
+  puts " Now walk it by yourself"
+  puts "#{moves_completed} move(s) completed!"
+  moves_completed+=1
+  #moves_completed
+end #end while
+end #end methods
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
