d960016b27a89d5d5541c9576c9bb6cec4a17933
caitlinmch <caitlinmch@me.com>
2018-06-05 20:48:23 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -1,17 +1,18 @@
-def dance 
+# def dance 
   
-  moves_completed=0 
-  loop do
-    if moves_completed==10 
-      break
-    end
-    puts "To the right, to the right"
-    puts "To the left, to the left"
-    puts "Now kick, now kick"
-    puts "Now walk it by yourself"
+#   moves_completed=0 
+#   loop do
+#     if moves_completed==10 
+#       break
+#     end
+#     puts "To the right, to the right"
+#     puts "To the left, to the left"
+#     puts "Now kick, now kick"
+#     puts "Now walk it by yourself"
     
-    moves_completed+=1 
-    puts "#{moves_completed} move(s) completed!"
-  end
-end
-  dance
+#     moves_completed+=1 
+#     puts "#{moves_completed} move(s) completed!"
+#   end
+# end
+#   dance
+# # end ends the method
